d9464e8b38e0d8678dd2df1f7af8e15c9f29cb6b
Joe Thompson <jthompson@hashicorp.com>
2020-07-07 01:56:31 -0400

    Initial commit of script.

--- README.md
+++ README.md
@@ -1,2 +1,178 @@
 # demo-magic-redux
-demoi-magic with some improvements to make demos more realistic
+@paxtonhare's [demo-magic](https://github.com/paxtonhare/demo-magic) with some improvements to make demos more realistic
+
+demo-magic.sh is a handy shell script that enables you to script repeatable demos in a bash environment so you don't have to type as you present. Rather than trying to type commands when presenting you simply script them and let demo-magic.sh run them for you.
+
+## Features
+- Simulates typing. It looks like you are actually typing out commands
+- Allows you to actually run commands or pretend to do so.
+- Can hide commands from presentation. Useful for behind the scenes stuff that doesn't need to be shown.
+
+## Functions
+
+### start_demo
+Clears the terminal and prints an initial prompt.
+
+### end_demo
+Prints a final blank line to make the return to the user's real prompt clean.
+
+### pe
+Print and Execute. This function will simulate typing whatever you give it. 
+
+```bash
+pe "ls -l"
+```
+
+### p
+Print only. This function will simulate typing whatever you give it. It will 
+not run the command.
+
+```bash
+p "ls -l"
+```
+
+By default both pe and p wait for a keypress before simulated typing (see 
+`wait` below).  You can configure whether and where these waits happen with 
+the NO_WAIT/-n and WAIT_AT/-a variables/options when invoking demo-magic.
+
+### pei
+Types and runs the given command immediately, with no waits.  Useful if you 
+want most commands to wait, but want to skip the waits between some (e.g. 
+when multiple commands are related to the same task and you want to execute 
+them as a group with no waits in between).
+
+```bash
+pei "ls -l"
+```
+
+### wait
+Waits for the user to press <kbd>ENTER</kbd>.  You can use this alone any 
+time you want the script to pause at an arbitrary point.
+
+If `PROMPT_TIMEOUT` is defined and > 0 the demo will automatically proceed after the amount of seconds has passed.
+
+```bash
+# Will wait until user presses enter
+PROMPT_TIMEOUT=0
+wait
+
+# Will wait max 5 seconds until user presses
+PROMPT_TIMEOUT=5
+wait
+```
+
+### cmd
+Enters script into interactive mode and allows newly typed commands to be executed within the script
+
+```
+cmd
+```
+
+## Getting Started
+Create a shell script and include demo-magic.sh
+
+```bash
+#!/bin/bash
+
+########################
+# include the magic
+########################
+. demo-magic.sh
+
+# hide the evidence
+start_demo
+
+# Put your stuff here
+```
+
+Then use the handy functions to run through your demo.
+
+## Command line usage
+demo-magic.sh exposes 6 options out of the box to your script.
+- `-h` - prints the usage text
+- `-a` - set where demo-magic waits when simulating typing (before, after, or both)
+- `-d` - disable simulated typing. Useful for debugging
+- `-n` - set no default waiting after `p` and `pe` functions
+- `-s` - set the simulated typing speed (default speed is 20)
+- `-w` - set no wait timeout after `p` and `pe` functions
+
+```bash
+$ ./demo-magic.sh -h
+
+Usage: ./demo-magic.sh [options]
+
+	Where options is one or more of:
+	-h	Prints Help text
+	-a	Whether to wait before or after simulated typing for user 
+		to hit enter (before, after, both)
+	-d	Debug mode. Disables simulated typing
+	-n	No wait
+	-s	Typing speed (default: 20)
+	-w	Waits max the given amount of seconds before proceeding 
+		with demo (e.g. '-w5')
+```
+
+## Useful Tricks
+
+Network connections during demos are often unreliable. Try and fake whatever 
+commands would rely on a network connection. For example: Instead of trying 
+to install node modules in a node.js application you can fake it. You can 
+install the node_modules at home on your decent network. Then rename the 
+directory and pretend to install it later by symlinking. If you want to be 
+thorough you can capture the output of npm install into a log file then cat 
+it out later to simulate the install.
+
+```bash
+#!/bin/bash
+
+########################
+# include the magic
+########################
+. demo-magic.sh
+
+# hide the evidence
+start_demo
+
+# this command is typed and executed
+pe "cd my-app"
+
+# this command is merely typed. Not executed
+p "npm install"
+
+# this command runs behind the scenes
+ln -s cached_node_modules node_modules
+
+# cat out a log file that captures a previous successful node modules install
+cat node-modules-install.log
+
+# now type and run the command to start your app
+pe "node index.js"
+
+# finish up
+end_demo
+```
+
+### No waiting
+The -n _no wait_ option can be useful if you want to print and execute multiple commands.
+
+```bash
+# include demo-magic
+. demo-magic.sh -n
+
+# add multiple commands
+pe 'git status'
+pe 'git log --oneline --decorate -n 20'
+```
+
+However this will oblige you to define your waiting points manually e.g.
+```bash
+...
+# define waiting points
+pe 'git status'
+pe 'git log --oneline --decorate -n 20'
+wait
+pe 'git pull'
+pe 'git log --oneline --decorate -n 20'
+wait
+```
+
---
+++ demo-magic.sh
@@ -0,0 +1,270 @@
+#!/usr/bin/env bash
+
+###############################################################################
+#
+# demo-magic.sh
+#
+# Copyright (c) 2015 Paxton Hare
+#
+# This script lets you script demos in bash. It runs through your demo script 
+# when you press ENTER. It simulates typing and runs commands.
+#
+###############################################################################
+
+# the speed to "type" the text
+TYPE_SPEED=20
+
+# no wait after "p" or "pe"
+NO_WAIT=false
+
+# wait only "before" typing, only "after" typing, or "both" before and after
+WAIT_AT="before"
+
+# if > 0, will pause for this amount of seconds before automatically proceeding with any p or pe
+PROMPT_TIMEOUT=0
+
+# don't show command number unless user specifies it
+SHOW_CMD_NUMS=false
+
+
+# handy color vars for pretty prompts
+BLACK="\033[0;30m"
+BLUE="\033[0;34m"
+GREEN="\033[0;32m"
+GREY="\033[0;90m"
+CYAN="\033[0;36m"
+RED="\033[0;31m"
+PURPLE="\033[0;35m"
+BROWN="\033[0;33m"
+WHITE="\033[1;37m"
+COLOR_RESET="\033[0m"
+
+C_NUM=0
+
+# prompt and command color which can be overriden
+DEMO_PROMPT="$ "
+DEMO_CMD_COLOR=$WHITE
+DEMO_COMMENT_COLOR=$GREY
+
+##
+# print the prompt
+##
+function magic_prompt() {
+  # render the prompt
+  x=$(PS1="$DEMO_PROMPT" "$BASH" --norc -i </dev/null 2>&1 | sed -n '${s/^\(.*\)exit$/\1/p;}')
+
+  # show command number is selected
+  if $SHOW_CMD_NUMS; then
+   printf "[$((++C_NUM))] '%s'" "$x"
+  else
+   printf '%s' "$x"
+  fi
+}
+
+##
+# prints the script usage
+##
+function usage() {
+  echo -e ""
+  echo -e "Usage: $0 [options]"
+  echo -e ""
+  echo -e "\tWhere options is one or more of:"
+  echo -e "\t-h\tPrints Help text"
+  echo -e "\t-a\tWhether to wait before or after simulated typing for user "
+  echo -e "\t\tto hit enter (before, after, both)"
+  echo -e "\t-d\tDebug mode. Disables simulated typing"
+  echo -e "\t-n\tNo wait"
+  echo -e "\t-s\tTyping speed (default: 20)"
+  echo -e "\t-w\tWaits max the given amount of seconds before proceeding "
+  echo -e "\t\twith demo (e.g. '-w5')"
+  echo -e ""
+}
+
+##
+# wait for user to press ENTER
+# if $PROMPT_TIMEOUT > 0 this will be used as the max time for proceeding 
+# automatically
+##
+function wait() {
+
+  if [[ "$PROMPT_TIMEOUT" == "0" ]]; then
+    read -rs
+  else
+    read -rst "$PROMPT_TIMEOUT"
+  fi
+
+}
+
+##
+# start the demo.  Clears the screen and prints a prompt.
+##
+
+function start_demo() {
+  clear
+  magic_prompt
+}
+
+##
+# end the demo.  Echoes a blank line without printing any command.
+##
+
+function end_demo() {
+  echo ""
+}
+
+##
+# print the command supplied for pe or p
+##
+function magic_print() {
+
+  if [[ ${1:0:1} == "#" ]]; then
+    cmd=$DEMO_COMMENT_COLOR$1$COLOR_RESET
+  else
+    cmd=$DEMO_CMD_COLOR$1$COLOR_RESET
+  fi
+
+  # wait for the user to press a key before typing the command
+  if ! $NO_WAIT && [[ "$WAIT_AT" =~ "both"|"before" ]]; then
+    wait
+  fi
+
+  if [[ -z $TYPE_SPEED ]]; then
+    echo -en "$cmd"
+  else
+    echo -en "$cmd" | pv -qL $(( TYPE_SPEED+(-2 + RANDOM%5) ));
+  fi
+
+  # wait for the user to press a key before moving on
+  if ! $NO_WAIT && [[ "$WAIT_AT" =~ "both"|"after" ]]; then
+    wait
+  fi
+  echo ""
+}
+
+##
+# execute the command supplied for pe
+##
+function magic_exec() {
+  eval "$@"
+}
+
+##
+# print command only. Useful for when you want to pretend to run a command
+#
+# takes 1 param - the string command to print
+#
+# usage: p "ls -l"
+#
+##
+function p() {
+  magic_print "$@"
+  magic_prompt
+}
+
+##
+# Prints and executes a command
+#
+# takes 1 parameter - the string command to run
+#
+# usage: pe "ls -l"
+#
+##
+function pe() {
+  # print the command
+  magic_print "$@"
+
+  # execute the command
+  magic_exec "$@"
+
+  magic_prompt
+}
+
+##
+# Prints and executes, but disables waiting for just this command
+#
+# takes 1 parameter - the string command to run
+#
+# usage: pei "ls -l"
+#
+##
+function pei() {
+  # print the command
+  NO_WAIT=true magic_print "$@"
+
+  # execute the command
+  magic_exec "$@"
+
+  magic_prompt
+}
+
+##
+# Enters script into interactive mode
+#
+# and allows newly typed commands to be executed within the script
+#
+# usage : cmd
+#
+##
+function cmd() {
+  # render the prompt
+  x=$(PS1="$DEMO_PROMPT" "$BASH" --norc -i </dev/null 2>&1 | sed -n '${s/^\(.*\)exit$/\1/p;}')
+  printf "%s\033[0m" "$x"
+  read command
+  eval "${command}"
+}
+
+
+function check_pv() {
+  command -v pv >/dev/null 2>&1 || {
+
+    echo ""
+    echo -e "${RED}##############################################################"
+    echo "# HOLD IT!! I require pv but it's not installed.  Aborting." >&2;
+    echo -e "${RED}##############################################################"
+    echo ""
+    echo -e "${COLOR_RESET}Installing pv:"
+    echo ""
+    echo -e "${BLUE}Mac:${COLOR_RESET} $ brew install pv"
+    echo ""
+    echo -e "${BLUE}Other:${COLOR_RESET} http://www.ivarch.com/programs/pv.shtml"
+    echo -e "${COLOR_RESET}"
+    exit 1;
+  }
+}
+
+check_pv
+#
+# handle some default params
+# -h for help
+# -d for disabling simulated typing
+#
+while getopts ":a:dhncw:s:" opt; do
+  case $opt in
+    h)
+      usage
+      exit 0
+      ;;
+    a)
+      WAIT_AT=$OPTARG
+      ;;
+    d)
+      unset TYPE_SPEED
+      ;;
+    n)
+      NO_WAIT=true
+      ;;
+    c)
+      SHOW_CMD_NUMS=true
+      ;;
+    w)
+      PROMPT_TIMEOUT=$OPTARG
+      ;;
+    s)
+      TYPE_SPEED=$OPTARG
+      ;;
+    *)
+      usage
+      exit 1
+      ;;
+  esac
+done
